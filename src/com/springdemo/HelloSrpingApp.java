f19364e49c28bb46a162da055bea9d6aef8920fc
tong <luck938938@hotmail.com>
2019-07-04 16:52:03 -0400

    initial commit

---
+++ src/com/springdemo/HelloSrpingApp.java
@@ -0,0 +1,25 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSrpingApp {
+
+	public static void main(String[] args) {
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach baseCoach = context.getBean("yourCoach", Coach.class);
+		Coach tableTennisCoach = context.getBean("tableTennisCoach", Coach.class);
+		
+		//call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(baseCoach.getDailyWorkout());
+		System.out.println(tableTennisCoach.getDailyWorkout());
+
+		//close the context
+		context.close();
+	}
+
+}
---
+++ src/com/springdemo/TableTennisCoach.java
@@ -0,0 +1,11 @@
+package com.springdemo;
+
+public class TableTennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Serve 100 times!";
+	}
+	
+}
